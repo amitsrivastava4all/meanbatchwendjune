04f251cf446ce013edf00b81133b85fa4d1aee37
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-07-20 11:32:10 +0530

    Code

---
+++ v2/shoppage/models/product.js
@@ -0,0 +1,13 @@
+class Product{
+    constructor(id ,name, price, desc, url){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.url = url;
+        this.desc = desc;
+        this.isAdded = false;
+    }
+    toggle(){
+        this.isAdded = !this.isAdded;
+    }
+}

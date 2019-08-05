0b06a72ad27f7c3bf8a69b669ee6674b1824ddb5
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-08-05 13:55:50 +0530

    Code

---
+++ firebasedemo/js/config.js
@@ -0,0 +1,12 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDkGVzo3UuTIA6pbI_zU4cBHwQXbpLdenU",
+    authDomain: "jsaugbatch.firebaseapp.com",
+    databaseURL: "https://jsaugbatch.firebaseio.com",
+    projectId: "jsaugbatch",
+    storageBucket: "",
+    messagingSenderId: "119590821497",
+    appId: "1:119590821497:web:e8f094c78818e97b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
